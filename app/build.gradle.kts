fb890a3fa1556d14f4bec6dfbe0593255f8d1f04
Dell <ua848948@gmail.com>
2026-06-16 10:48:25 -0700

    splash Screen Add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 android {
     namespace = "com.sprizen.uashoppingcenter"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
---
+++ app/src/main/java/com/sprizen/uashoppingcenter/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.sprizen.uashoppingcenter
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.sprizen.uashoppingcenter.appenterlogin.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val logo = findViewById<LinearLayout>(R.id.linearLayout)
+
+        val animation =
+            AnimationUtils.loadAnimation(this, R.anim.zoom_in_out)
+
+        logo.startAnimation(animation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            startActivity(
+                Intent(this, LoginActivity::class.java)
+            )
+
+            finish()
+
+        }, 2000)
+    }
+}
