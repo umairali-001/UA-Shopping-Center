ebf7f2d5bdae0166b8712c0f0a77681a1a905427
Dell <ua848948@gmail.com>
2026-06-22 21:43:27 -0700

    Tab Layout ko fix keya hai

--- app/src/main/java/com/sprizen/uashoppingcenter/Activities/HomeActivity.kt
+++ app/src/main/java/com/sprizen/uashoppingcenter/Activities/HomeActivity.kt
@@ -5,6 +5,8 @@ import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
@@ -37,6 +39,9 @@ class HomeActivity : AppCompatActivity() {
     //===================================================================================================================
 
     fun initializeEveryThing(){
+
+
+
 
 
         imageList = ArrayList()
